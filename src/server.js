1134b1cfdf84be283864483121964a8cc30dc4d2
unpieceof <unpieceof@gmail.com>
2022-06-15 22:54:51 +0900

    [Update] message만 backend에서 처리되도록 수정

--- src/server.js
+++ src/server.js
@@ -26,7 +26,10 @@ wss.on("connection", (socket) => {
     console.log("Connected to Browser ✅");
     socket.on("close", onSocketClose);
     socket.on("message", (message) => {
-        sockets.forEach((aSocket) => aSocket.send(message.toString()));
+        const parsed = JSON.parse(message);
+        if(parsed.type === "new_message"){
+            sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+        }
     });
 });
 

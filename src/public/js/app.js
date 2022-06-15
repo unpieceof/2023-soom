d090546fa2076fa4df56930596f38a5cd931d0e8
unpieceof <unpieceof@gmail.com>
2022-06-15 22:34:01 +0900

    [Update] nickname, message 구분하여 전송

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,13 @@
 // socket = 서버로의 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 function handleOpen() {
     console.log("Connected to Server ✅");
@@ -23,7 +29,15 @@ socket.addEventListener("close", () => {
 function handleSubmit(event){
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
-    input.value = '';
+    socket.send(makeMessage("new_message",input.value));
+    input.value = "";
 }
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);

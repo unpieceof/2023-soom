cfa3937c43650b8b234f607f278a6ceb4623ef62
unpieceof <unpieceof@gmail.com>
2022-06-19 18:40:05 +0900

    [Fix] app.js function 수정

--- src/server.js
+++ src/server.js
@@ -10,8 +10,6 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
@@ -24,4 +22,5 @@ wsServer.on("connection", (socket) => {
     });
 });
 
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);

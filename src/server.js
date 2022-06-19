dea77cd3782fe168a019a93ddbce35d0ec219516
unpieceof <unpieceof@gmail.com>
2022-06-19 18:37:51 +0900

    [Update] ws to socket.io

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -12,33 +12,16 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-function onSocketClose() {
-    console.log("Disconnected from Browser ❌");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type){
-            case "new_message":
-                sockets.forEach((aSocket) => 
-                    aSocket.send(`${socket.nickname}: ${message.payload}`)
-                );
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break;
-        }
+wsServer.on("connection", (socket) => {
+    socket.on("enter_room", (msg,done) => {
+        console.log(msg);
+        setTimeout(() => {
+            done();
+        }, 10000);
     });
 });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);

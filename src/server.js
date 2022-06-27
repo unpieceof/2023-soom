3189b1ac65dc0af77f6237de70f187753740b3bb
unpieceof <unpieceof@gmail.com>
2022-06-27 23:17:28 +0900

    [Update] 필요없는 console.log 삭제

--- src/server.js
+++ src/server.js
@@ -18,12 +18,7 @@ wsServer.on("connection", (socket) => {
         console.log(`Socket Event :${event}`);
     });
     socket.on("enter_room", (roomName, done) => {
-        console.log(socket.rooms);
         socket.join(roomName);
-        console.log(socket.rooms);
-        setTimeout(() => {
-            done("hello from the backend");
-        }, 10000)
     });
 });
 
